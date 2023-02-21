f4824a4bf97b9e0f3fc06c8434b6734b5ae75893
qa-zuonv-4-akxf@example.com <qa-zuonv-4-akxf@example.com>
2023-02-21 21:59:07 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15356.stoplight-dev.com/api/v1/projects/cHJqOjE3NA/spectral.js?branch_slug=main&token=f89214ec-3f06-4082-8fd0-a11fc12e723d";
+export default { extends: ruleset };
